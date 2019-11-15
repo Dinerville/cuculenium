c9f9be67a232996a3744159f5f4a5042fd28d9b1
Dmitry Naumov <nadima8@gmail.com>
2019-11-15 18:09:33 +0300

    add vs code

--- Cuculenium/Steps/CalculatorSteps.steps.cs
+++ Cuculenium/Steps/CalculatorSteps.steps.cs
@@ -14,5 +14,34 @@ namespace Framework
 			When($"I click element on 'mainPage' named 'searchButton'");
 		}
 
+
+		[Given("I have entered (.*) into the my name textbox")]
+		
+		public void IHaveEnteredIntoTheMyNameTextbox(string aName)
+		{
+			
+			When($"I set text {aName} for element on mainPage with name nameTextBox");
+		}
+
+
+		[When("I press add")]
+		[Given("I press add")]
+		
+		public void IPressAdd()
+		{
+			
+			When($"I click element on mainPage with name addButton");
+		}
+
+
+		[Then("the result should be (.*) on the screen")]
+		
+		public void TheResultShouldBeOnTheScreen(string expectedResult)
+		{
+			
+			When($"I get text of element on mainPage with name resultLabel as <@SetLocal actualResult>");
+			Then($"I assert that {expectedResult} equals to <@GetLocal actualResult>. If not say 'Expected result is not equal to actual'");
+		}
+
   }
 }
